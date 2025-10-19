d234e3bbd55e7ffb43901154019ba2e15b66dddd
Jurriaan Roelofs <jur@dxpr.com>
2025-10-19 15:19:14 +0200

    test: verify pre-commit hook works correctly

---
+++ test_hook.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Test file with intentional coding violations.
+ */
+
+/**
+ * Test function to demonstrate hooks.
+ */
+function test_function($param1, $param2) {
+  $result = $param1 + $param2;
+  return $result;
+}
